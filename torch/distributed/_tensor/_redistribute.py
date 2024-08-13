db55565bc85fa005e884ff3e7cff7dc5656d8d1f
botbw <wang1570@e.ntu.edu.sg>
2024-08-13 07:27:36 +0000

    [dtensor] init shard to shard (and replicate) comm

--- torch/distributed/_tensor/_redistribute.py
+++ torch/distributed/_tensor/_redistribute.py
@@ -1,10 +1,14 @@
 # mypy: allow-untyped-defs
 # Copyright (c) Meta Platforms, Inc. and affiliates
 import logging
+from copy import deepcopy
 from functools import lru_cache
-from typing import cast, List, NamedTuple, Tuple
+from typing import Dict, cast, List, NamedTuple, Tuple, Optional
+from collections import defaultdict
+from dataclasses import dataclass
 
 import torch
+import torch.distributed as dist
 import torch.distributed._functional_collectives as funcol
 import torch.distributed._tensor.api as dtensor
 from torch.distributed._tensor.device_mesh import DeviceMesh
@@ -69,7 +73,7 @@ def _gen_transform_infos(
     # Handle multi-dim device mesh placement redistribution
     # First, we need to build the logical shape for each mesh dim
     # for correct allgathering uneven shards on each mesh dim (with dynamic padding)
-    for i, (src, dst) in enumerate(zip(src_spec.placements, dst_spec.placements)):
+    for i, (src, _) in enumerate(zip(src_spec.placements, dst_spec.placements)):
         current_logical_shape = mesh_dims_to_logical_shape[i]
         if isinstance(src, Shard):
             if i < device_mesh.ndim - 1:
@@ -147,7 +151,7 @@ def _gen_transform_infos(
     return transform_infos
 
 
-def redistribute_local_tensor(
+def redistribute_local_tensor_(
     local_tensor: torch.Tensor,
     current_spec: DTensorSpec,
     target_spec: DTensorSpec,
@@ -180,7 +184,6 @@ def redistribute_local_tensor(
     for transform_info in transform_infos:
         i = transform_info.mesh_dim
         current, target = transform_info.src_dst_placements
-        num_chunks = device_mesh.size(mesh_dim=i)
 
         if current == target:
             # short cut, just use the original local tensor
@@ -264,6 +267,7 @@ def redistribute_local_tensor(
         assert new_local_tensor is not None
         local_tensor = new_local_tensor
 
+    del local_tensor
     assert new_local_tensor is not None, "redistribute failed!"
 
     if not async_op and isinstance(new_local_tensor, funcol.AsyncCollectiveTensor):
@@ -350,3 +354,127 @@ class Redistribute(torch.autograd.Function):
             None,
             None,
         )
+
+
+@dataclass
+class Partition:
+    '''
+        Tensor partition from a logical view, and the rank that holds it.
+    '''
+    start_coord: Tuple[int, ...]  # start coord of this partition
+    end_coord: Tuple[int, ...]  # end coord of this partition
+    rank: int  # rank that hold this partition
+
+    def __repr__(self) -> str:
+        return f"{{{self.start_coord}->{self.end_coord} on rank{self.rank}}}"
+
+    def shard(self, tensor_dim: int, shard_num: int, shard_idx: int) -> "Partition":
+        if (self.end_coord[tensor_dim] - self.start_coord[tensor_dim]) % shard_num != 0:
+            raise ValueError(f"shard_num must be a factor of the partition size, found {shard_num=} {self.end_coord[tensor_dim]=} {self.start_coord[tensor_dim]=}")
+
+        block_size = (self.end_coord[tensor_dim] - self.start_coord[tensor_dim]) // shard_num
+        return Partition(
+            self.start_coord[:tensor_dim] + (self.start_coord[tensor_dim] + block_size * shard_idx,) + self.start_coord[tensor_dim+1:],
+            self.end_coord[:tensor_dim] + (self.start_coord[tensor_dim] + block_size * (shard_idx + 1),) + self.end_coord[tensor_dim+1:],
+            self.rank
+        )
+
+    @staticmethod
+    def from_tensor_spec(spec: DTensorSpec) -> Tuple["Partition"]:
+        if spec.tensor_meta is None:
+            raise ValueError("tensor_meta is not set")
+
+        tensor_mesh = spec.mesh.mesh
+        global_shape, _, _ = spec.tensor_meta
+        placements = spec.placements
+
+        unravel = torch.unravel_index(torch.arange(tensor_mesh.numel()), tensor_mesh.shape)
+        rank_to_coord = []
+        for i in range(unravel[0].shape[0]):
+            rank_to_coord.append(
+                tuple(unravel[j][i].item() for j in range(tensor_mesh.ndim))
+            )
+        partitions = [
+            Partition((0,) * len(global_shape), tuple(global_shape), rank) for rank in tensor_mesh.flatten().tolist()
+        ]
+
+        for mesh_dim, placement in enumerate(placements):
+            if not placement.is_shard():
+                continue
+            shard = cast(Shard, placement)
+            tensor_dim = shard.dim
+            shard_num = tensor_mesh.size(mesh_dim)
+            for partition in partitions:
+                partitions[partition.rank] = partition.shard(tensor_dim, shard_num, rank_to_coord[partition.rank][mesh_dim])
+
+        return tuple(partitions)
+
+    def from_src(self, src_partition: "Partition") -> Optional["Partition"]:
+
+        start_coord = tuple(max(s1, s2) for s1, s2 in zip(self.start_coord, src_partition.start_coord))
+        end_coord = tuple(min(e1, e2) for e1, e2 in zip(self.end_coord, src_partition.end_coord))
+        if any(s >= e for s, e in zip(start_coord, end_coord)):
+            return None
+
+        return Partition(start_coord, end_coord, src_partition.rank)
+
+    @staticmethod
+    def gen_p2p_op(rank: int, src_tensor: torch.Tensor, src_partitions: Tuple["Partition"], tgt_partitions: Tuple["Partition"]) -> Tuple[List[dist.P2POp], List[dist.P2POp], torch.Tensor, List[Tuple[slice]]]:
+        local_src_partition = src_partitions[rank]
+        local_dst_partition = tgt_partitions[rank]
+
+        recv_info = defaultdict(list)
+        for tgt in tgt_partitions:
+            cache = defaultdict(int)
+            # TODO better load balance strategy
+            for src in sorted(src_partitions, key=lambda x: abs(x.rank - tgt.rank)):
+                intersection = tgt.from_src(src)
+                if intersection is None:
+                    continue
+                cache_str = f"{intersection.start_coord}{intersection.end_coord}"
+                if cache_str not in cache:
+                    recv_info[tgt.rank].append(intersection)
+                cache[cache_str] += 1
+
+        send_ops = []
+        recv_ops = []
+        recv_slices = []
+        buffer_shape = tuple(e - s for s, e in zip(local_dst_partition.start_coord, local_dst_partition.end_coord))
+        recv_buffer = torch.empty(buffer_shape, dtype=src_tensor.dtype, device=src_tensor.device) * -10086
+        for recv_rank, intersections in recv_info.items():
+            for intersection in intersections:
+                send_rank = intersection.rank
+                src_slice = tuple(slice(st - base, en - base) for base, st, en in zip(local_src_partition.start_coord, intersection.start_coord, intersection.end_coord))
+                tgt_slice = tuple(slice(st - base, en - base) for base, st, en in zip(local_dst_partition.start_coord, intersection.start_coord, intersection.end_coord))
+
+                if rank == send_rank == recv_rank:
+                    # assign the static intersection to the buffer
+                    recv_buffer[tgt_slice] = src_tensor[src_slice]
+                    continue
+
+                if rank == send_rank:
+                    src_slice = tuple(slice(st - base, en - base) for base, st, en in zip(local_src_partition.start_coord, intersection.start_coord, intersection.end_coord))
+                    send_ops.append(dist.P2POp(dist.isend, src_tensor[src_slice].contiguous(), recv_rank))
+
+                if rank == recv_rank:
+                    tgt_slice = tuple(slice(st - base, en - base) for base, st, en in zip(local_dst_partition.start_coord, intersection.start_coord, intersection.end_coord))
+                    recv_ops.append(dist.P2POp(dist.irecv, recv_buffer[tgt_slice].contiguous(), send_rank))
+                    recv_slices.append(tgt_slice)
+
+        # TODO: handle the case where there is no communication, currently sending a dummy tensor
+        if len(send_ops + recv_ops) == 0:
+            # NOTE: recv_buffer, recv_slices will still be empty
+            dummy_tensor = torch.zeros(1, dtype=src_tensor.dtype, device=src_tensor.device)
+            send_ops.append(dist.P2POp(dist.isend, dummy_tensor, rank))
+            recv_ops.append(dist.P2POp(dist.irecv, dummy_tensor, rank))
+
+        return send_ops, recv_ops, recv_buffer, recv_slices
+
+    @staticmethod
+    def fill_recv_buffer(recv_ops: List[dist.P2POp], recv_buffer: torch.Tensor, recv_slices: List[Tuple[slice]]) -> torch.Tensor:
+        for op, slice in zip(recv_ops, recv_slices):
+            if op.op is not dist.irecv:
+                raise ValueError("recv_ops must be irecv")
+            recv_buffer[slice] = op.tensor
+
+        return recv_buffer
